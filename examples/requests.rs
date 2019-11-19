8a1f1f2c04fd3a31863e5a7accac3bd1339cd858
Dhole <dhole@riseup.net>
2019-11-19 20:51:32 +0100

    WIP 2

--- examples/requests.rs
+++ examples/requests.rs
@@ -1,7 +1,7 @@
 use std::io;
 use urpc;
 
-use postcard::from_bytes;
+use postcard::{from_bytes, to_slice};
 // urpc::setup!(
 //     methods: [
 //         {id: 0, name: SendBytes, request: [u8]},
@@ -36,8 +36,32 @@ enum Error {
 //     SendBytes(SendBytesRequestBody<'a>),
 // }
 enum Request<'a> {
-    Ping([u8; 4]),
-    SendBytes(&'a [u8]),
+    Ping(RequestPing),
+    SendBytes(RequestSendBytes<'a>),
+}
+
+struct RequestPing {
+    header: urpc::RequestHeader,
+    body: [u8; 4],
+}
+
+impl RequestPing {
+    pub fn reply(self, payload: [u8; 4]) -> () {
+        ()
+    }
+    pub fn reply_err(self, err: u8) -> () {}
+}
+
+struct RequestSendBytes<'a> {
+    header: urpc::RequestHeader,
+    body: &'a [u8],
+}
+
+impl<'a> RequestSendBytes<'a> {
+    pub fn reply(self, payload: ()) -> () {
+        ()
+    }
+    pub fn reply_err(self, err: u8) -> () {}
 }
 
 // Auto
@@ -47,16 +71,22 @@ enum ReplyBody {
 }
 
 // Auto
-enum Reply {
+enum Reply<T> {
     Ack,
     Error(Error),
-    Body(ReplyBody),
+    Body(T),
 }
 
-fn req_body_from_bytes<'a>(header: &urpc::RequestHeader, buf: &'a [u8]) -> Request<'a> {
+fn req_body_from_bytes<'a>(header: urpc::RequestHeader, buf: &'a [u8]) -> Request<'a> {
     match header.method_idx {
-        0 => Request::Ping(from_bytes::<[u8; 4]>(buf).unwrap()),
-        1 => Request::SendBytes(from_bytes::<&'a [u8]>(buf).unwrap()),
+        0 => Request::Ping(RequestPing {
+            header: header,
+            body: from_bytes(buf).unwrap(),
+        }),
+        1 => Request::SendBytes(RequestSendBytes {
+            header: header,
+            body: from_bytes(buf).unwrap(),
+        }),
         _ => {
             unreachable!();
         }
@@ -71,7 +101,9 @@ struct RpcClient;
 
 impl RpcClient {
     fn ping(&self, data: &[u8; 4]) -> Result<[u8; 4], io::Error> {
-        Ok([0; 4]) // Example
+        let mut echo = [0; 4];
+        echo.copy_from_slice(data);
+        Ok(echo)
     }
 
     fn send_bytes(&self, bytes: &[u8]) -> Result<(), io::Error> {
@@ -79,14 +111,26 @@ impl RpcClient {
     }
 }
 
+// trait Rpc {
+//     type Request;
+//     type Reply;
+//
+//     fn
+// }
+
 fn main() -> () {
     let req_header_buf = [0; 5];
     let req_header = urpc::req_header_from_bytes(&req_header_buf);
 
     let req_body_buf = vec![0; req_header.length as usize];
-    let req_body = req_body_from_bytes(&req_header, &req_body_buf[..]);
-    let reply = match req_body {
-        Request::SendBytes(_body) => Reply::Ack,
-        Request::Ping(body) => Reply::Body(ReplyBody::Ping(body)),
+    let req = req_body_from_bytes(req_header, &req_body_buf[..]);
+    let reply = match req {
+        Request::SendBytes(send_bytes) => send_bytes.reply(()),
+        Request::Ping(ping) => {
+            let body = ping.body.clone();
+            ping.reply(body)
+        }
     };
+    let mut reply_buf = [0; 32];
+    to_slice(&reply, &mut reply_buf);
 }
